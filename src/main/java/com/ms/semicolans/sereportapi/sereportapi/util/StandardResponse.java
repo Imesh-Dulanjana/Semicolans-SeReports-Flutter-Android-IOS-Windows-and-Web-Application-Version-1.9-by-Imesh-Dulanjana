453807b1cc8be93ee7f8c97fd7b7193c6aa316df
Imesh-Dulanjana <imeshdulanjana1413@gmail.com>
2026-05-17 09:36:28 +0530

    updated

--- src/main/java/com/ms/semicolans/sereportapi/sereportapi/util/StandardResponse.java
+++ src/main/java/com/ms/semicolans/sereportapi/sereportapi/util/StandardResponse.java
@@ -3,13 +3,12 @@ package com.ms.semicolans.sereportapi.sereportapi.util;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StandardResponse {
-    private int code;
+    private int status;
     private String message;
     private Object data;
-
-
 }
